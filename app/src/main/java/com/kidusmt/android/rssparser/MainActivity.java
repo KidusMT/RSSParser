f588739fec88d2cbf5de226e1838142db939984f
adu.kidus@yahoo.com <[adu.kidus@yahoo.com]>
2017-08-02 08:42:48 +0300

    can populate more cardview

--- app/src/main/java/com/kidusmt/android/rssparser/MainActivity.java
+++ app/src/main/java/com/kidusmt/android/rssparser/MainActivity.java
@@ -7,14 +7,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_rss;
     RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_rss = (TextView)findViewById(R.id.tv_main);
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
 
         ReadRSS rss = new ReadRSS(this,recyclerView);
