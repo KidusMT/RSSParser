e1dd74a25a384abff577c08cba67db79b377c468
adu.kidus@yahoo.com <[adu.kidus@yahoo.com]>
2017-08-04 04:38:07 +0300

    save and download image worked in AsyncTask

---
+++ app/src/main/java/com/kidusmt/android/rssparser/DownloadImage.java
@@ -0,0 +1,10 @@
+package com.kidusmt.android.rssparser;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by KidusMT on 8/2/2017.
+ */
+
+public class DownloadImage extends AsyncTask {
+}
---
+++ app/src/main/java/com/kidusmt/android/rssparser/RecyclerItemClickListener.java
@@ -0,0 +1,10 @@
+package com.kidusmt.android.rssparser;
+
+import RecyclerView.OnItemTouchListener;
+
+/**
+ * Created by KidusMT on 8/2/2017.
+ */
+
+public class RecyclerItemClickListener implements OnItemTouchListener {
+}
